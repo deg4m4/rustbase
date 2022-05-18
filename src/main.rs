9857e67a5fd27d84622022e4c576166e5c9579ed
elparthka <parthka.2005@gmail.com>
2022-05-18 11:16:56 +0530

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
 
-    for c in "parth".bytes() {
-        println!("{}: {}", String::from_utf8_lossy(&[c]), c);
+    let mut person = HashMap::new();
+    person.insert(String::from("name"), String::from("Parthka"));
+    person.insert(String::from("username"), String::from("meparthka"));
+    person.insert(String::from("age"), String::from("17"));
+    person.insert(String::from("phone"), String::from("+91 886 688 1066"));
+    person.entry(String::from("color")).or_insert("Dark-Light".to_string());
+
+    for w in "I Am Parth".split_whitespace() {
+        let c = person.entry(w.to_string()).or_insert("123".to_string());
+        println!("{}", c);
     }
 
-}
+    println!("{:?}", person);
 
-#[allow(dead_code)]
-fn pushing(p: &mut String) {
-    p.push_str(" pop");
 }
