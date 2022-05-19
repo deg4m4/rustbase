48f624ec791164d44ae874aec3616bf281298c87
meparthka <parthka.2005@gmail.com>
2022-05-19 11:27:58 +0530

    Generic type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Add<T, U> {
+    m:U,
+    l:T,
+}
+
+impl<T, U> Add<T, U>{
+    fn mgni(&self) -> &T {
+        &self.l
+    }
+
+    fn leti(&self) -> &U {
+        &self.m
+    }
+}
+
 fn main() {
-    
+
+    let p = Add{m:122.3, l:43};
+
+    println!("{}", p.mgni());
+    println!("{}", p.leti());
+
 }
