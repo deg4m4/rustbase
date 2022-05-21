7b03dfe57aa9aaf4dccef0e340a9591b2b8ca640
meparthka <parthka.2005@gmail.com>
2022-05-21 10:16:39 +0530

    lifetime referanc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
 
+    let n1 = "pkk";
+    let result;
+    let n2 = "pka";
+    result = longest(&n1, &n2);  
+    println!("Longest: {}", result);
+
+}
+
+fn longest<'ab, T>(n1: &'ab T, n2: &'ab T) -> T
+where T: PartialOrd + Copy
+{
+    if n1 > n2 {
+        *n1
+    } else {
+        *n2
+    }
 }
