9b9c9aabe322232f79f9700ac6ad9f4c0d76aed0
meparthka <parthka.2005@gmail.com>
2022-05-21 09:36:17 +0530

    clear project

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-mod modal;
-
-use modal::{Location, LocInfo}; 
-
 fn main() {
-    let p = Location {
-        x_axis: 32,
-        y_axis: 54.58,
-        tree_age: vec![12, 43, 45, 21, 21],
-    };
-    println!("X Axis: {}", p.get_x());
-    println!("Y Axis: {}", p.get_y());
-    p.print_y();
-    p.print_x();
-    println!("Max Age: {}", p.get_max_age());
-    let p = Location {
-        x_axis: 32,
-        y_axis: 43,
-        tree_age: vec!['a', 'f', '😻', 'y', 'm'],
-    };
-    println!("X Axis: {}", p.get_x());
-    println!("Y Axis: {}", p.get_y());
-    p.print_y();
-    p.print_x();
-    let m: char = *p.get_max_age();
-    println!("{}", m);
+
 }
