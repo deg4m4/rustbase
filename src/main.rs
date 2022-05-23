dda75f236a7f7b4d5a1ce829d9e122aa7165124d
meparthka <parthka.2005@gmail.com>
2022-05-23 17:29:58 +0000

    command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,56 @@
 use std::env;
 use std::fs;
+use std::error::Error;
 
 fn main() {
     let arg: Vec<String> = env::args().collect();
-
-    let f = Args{file: "".to_string(), name: "".to_string(), n:12};
-
-    let s: Args<i32> = f.get_arg(&arg);
-
-    let file = match fs::read_to_string(f.file) {
-        Ok(c) => c,
-        Err(_e) => "Not File Found".to_string(),
+    let l = match Args::get_arg(&arg) {
+        Ok(n) => n,
+        Err(_e) => panic!("Sorry, {}", _e),
     };
 
-    println!("{}", file);
+    if let Err(e) = l.print_text(){
+        println!("{}", e);
+    }
+
+    let p = l.print_text();
+    println!("{}", p.unwrap());
+
+    println!("{}", "Thanks For One Time...")
 }
 
 struct Args<T> {
     name: T,
     file: T,
-    n: T
 }
 
 trait ArgsFunc<T> {
-    fn get_arg(&self, a: &Vec<String>) -> Args<&T>;
+    fn get_arg(a: &Vec<T>) -> Result<Args<&T>, String>;
+    fn print_text(&self) ->  Result<String, Box<dyn Error>>;
 }
 
-impl<T> ArgsFunc<T> for Args<T> 
+impl<T> ArgsFunc<T> for Args<T>
+where T: std::fmt::Display + std::convert::AsRef<std::path::Path>
 {
-    fn get_arg(&self, a: &Vec<String>) -> Args<&T> {
-            Args { name: String::from("wdasd"), file: String::from("wdasd"), n: String::from("wdasd") }
+    fn get_arg(a: &Vec<T>) -> Result<Args<&T>, String> {
+        if a.len() < 3 {
+            return Err(format!("{}", "Give Three Or More Arguments..."));
         }
+        let file = &a[2];
+        let name = &a[1];
+        Ok(Args { name, file })
+    }
+
+    fn print_text(&self) -> Result<String, Box<dyn Error>>{
+        println!("{}", self.file);
+        println!("{}", self.name);
+    
+        let file = match fs::read_to_string(&self.file) {
+            Ok(c) => c,
+            Err(_e) => "Not File Found".to_string(),
+        };
+    
+        println!("{}", file);
+        Ok(String::from("Success"))
+    }
 }
