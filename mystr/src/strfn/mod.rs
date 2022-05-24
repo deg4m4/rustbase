3faf9c4a58c209e08b158e2c744eb55f393e467e
meparthka <parthka.2005@gmail.com>
2022-05-24 10:41:09 +0530

    clear project

--- mystr/src/strfn/mod.rs
+++
@@ -1,9 +0,0 @@
-pub fn search<'a>(q: &'a str, s: &'a str) -> Vec<&'a str> {
-    let mut ls = Vec::new();
-    for l in s.lines() {
-        if l.contains(q) {
-            ls.push(l);
-        }
-    }
-    ls
-}
