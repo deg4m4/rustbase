904239cbd7efd37f12a79f6b97690c709205b019
meparthka <parthka.2005@gmail.com>
2022-05-30 00:23:35 +0530

    clear projects

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use std::thread;
-use std::time::Duration;
-
 fn main() {
-  for i in 1..10 {
-   thread::spawn(move || {
-      thread::sleep(Duration::from_millis(i * 1000));
-      println!("hi number {} from the spawned thread!", i);
-      /* if i == 9 {
-        std::process::exit(0);
-      } */
-    });
-  }
-  loop{}
+
 }
