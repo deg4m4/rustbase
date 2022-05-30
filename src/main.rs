78c681df69c6941099b3ce4b3cc475618a9c8f4c
meparthka <parthka.2005@gmail.com>
2022-05-30 09:16:54 +0530

    clear project

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-
 fn main() {
-  let lc = Arc::new(Mutex::new(0));
-  thread::spawn(move || {
-    for i in 0..10 {
-      thread::sleep(std::time::Duration::from_millis(
-        (i * 1000).try_into().unwrap(),
-      ));
-      *lc.lock().unwrap() += 1;
-      println!("Hello, World!");
-      println!("{}", lc.lock().unwrap());
-    }
-  });
-  loop {}
+  
 }
